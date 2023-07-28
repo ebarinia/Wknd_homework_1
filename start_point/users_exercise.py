900925a955a6be398a9281d4befa23c2f454f19c
Emmanuel <Adrianna@Emmanuels-MBP.broadband>
2023-07-28 14:57:36 +0100

    second commit

--- start_point/users_exercise.py
+++ start_point/users_exercise.py
@@ -55,12 +55,29 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+print(users["Jonathan"]["twitter"])
 # 2. Get Erik's hometown
+print(users["Erik"]["home_town"])
 # 3. Get the list of Erik's lottery numbers
+print(users["Erik"]["lottery_numbers"])
 # 4. Get the species of Avril's pet Monty
+print(users["Avril"]["pets"][0]["species"])
 # 5. Get the smallest of Erik's lottery numbers
-# 6. Return an list of Avril's lottery numbers that are even
+numbers = users["Erik"]["lottery_numbers"]
+numbers_sorted = sorted(numbers)[:1]
+
+print(numbers_sorted)
+# 6. Return a list of Avril's lottery numbers that are even
+avril_numbers = users["Avril"]["lottery_numbers"]
+even_numbers = []
+
+for number in avril_numbers:
+    if (number % 2) == 0:
+        even_numbers.append(number)
+
+print(even_numbers)
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "fluffy"
 # 10. Add another person to the users dictionary
