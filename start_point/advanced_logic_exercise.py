5b946c8d6fb847bad49da0626623140ba855c60f
Emmanuel <Adrianna@Emmanuels-MacBook-Pro.local>
2023-07-31 10:42:22 +0100

    final submission

--- start_point/advanced_logic_exercise.py
+++ start_point/advanced_logic_exercise.py
@@ -2,30 +2,54 @@
 
 numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 
-# # 1. Print out a list of the even integers:
-# ordered_num = sorted(numbers)
-# print(ordered_num)
+# 1. Print out a list of the even integers:
+ordered_num = []
 
-# # 2. Print the difference between the largest and smallest value:
+for num in numbers:
+    if num % 2 == 0:
+        ordered_num.append(num)
+
+print(ordered_num)
+
+# 2. Print the difference between the largest and smallest value:
 # largest_num = sorted(numbers, reverse=True)[0]
 # smallest_num = ordered_num[0]
 
 # print(largest_num - smallest_num)
 
-# # 3. Print True if the list contains a 2 next to a 2 somewhere.
-identical = False
+# ---------
+largest = max(numbers)
+smallest = min(numbers)
+print(largest - smallest)
 
-for number in range(len(numbers)):
-    print(numbers[number])
-    for two in number:
-        if two == number:
-            identical = True
-print(identical)
+# 3. Print True if the list contains a 2 next to a 2 somewhere.
+result = False
+index = 0
+
+for num in numbers:
+    if num == 2 and numbers[index - 1] == 2:
+        result = True
+    index += 1
+print(result)
 
 # 4. Print the sum of the numbers, 
 #    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
 #    
 #    So [11, 6, 4, 99, 7, 11] would have sum of 22
+
+total = 0
+found_6 = False
+for num in numbers:
+    if num == 6:
+        found_6 = True
+    elif found_6 == True:
+        if num == 7:
+            found_6 = False
+    
+    else:
+        total += num
+
+print(total)
 
 
 # 5. HARD! Print the sum of the numbers. 
@@ -34,6 +58,17 @@ print(identical)
 #    HINT - You will need to track the index throughout the loop.
 #
 #    So [5, 13, 2] would have sum of 5. 
+
+index = 0
+total = 0
+for num in numbers:
+    if num == 13 or numbers[index - 1] == 13:
+        pass
+    else:
+        total += num
+    index += 1
+
+print(total)
 
 
 
